c5a6ccd4e27a758704e215f21fd9782e5ad5a45e
Aung Sann Thit <90388214+AungSannThit2000@users.noreply.github.com>
2023-01-17 22:39:57 +0630

    edited main.py

--- main.py
+++ main.py
@@ -13,30 +13,34 @@ from glob import glob
 
 detector = HandDetector(detectionCon=0.8, maxHands=2)
 
-
 offset = 50
 size = (300, 300)
 hand_features = []
 
-# def process(image):
-#     hands_crop, image_crop = detector.findHands(image.copy())
-#     if hands_crop:
-#         hand_crop = hands_crop[0]
-#         x, y, w, h = hand_crop['bbox']
-#         img_crop = image[y - offset: y + h + offset, x - offset: x + w + offset]
-#
-#         img_crop = cv2.resize(img_crop, size)  # Tune the image size
-#         hands, img = detector.findHands(img_crop.copy())
-#         if hands:
-#             hand = hands[0]
-#             hand_lm = hand['lmList']
-#             hand_lm = np.array(hand_lm).flatten()
-#             x, y, w, h = hand['bbox']
-#             hand_features.append(hand_lm)
-#
-#         cv2.imshow("Cropped Image", img)
-#     cv2.imshow("Original Image", image_crop)
-#     return image_crop
+
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+        f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+        unsafe_allow_html=True
+    )
+
+
+add_bg_from_local('bg_5hands.jpg')
+
+st.header(':gray[_Welcome to NyoKi Classifier_]')
+st.subheader('Hand Sign Recognition Application (Word Level)')
+activities = ["Home", "Webcam Hand Detection", "Video File Hand Detection", "Thanks"]
+choice_s = st.sidebar.selectbox("Select Activity <3", activities)
+
 
 def process(image):
     hands_crop, image_crop = detector.findHands(image)
@@ -45,13 +49,8 @@ def process(image):
         x, y, w, h = hand_crop['bbox']
         img_crop = image[y - offset: y + h + offset, x - offset: x + w + offset]
 
-
-    #cv2.imshow("Original Image", image_crop)
+    # cv2.imshow("Original Image", image_crop)
     return image_crop
-
-RTC_CONFIGURATION = RTCConfiguration(
-    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
-)
 
 
 class VideoProcessor:
@@ -63,11 +62,24 @@ class VideoProcessor:
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 
-webrtc_ctx = webrtc_streamer(
-    key="WYH",
-    mode=WebRtcMode.SENDRECV,
-    rtc_configuration=RTC_CONFIGURATION,
-    media_stream_constraints={"video": True, "audio": False},
-    video_processor_factory=VideoProcessor,
-    async_processing=True,
-)
+if choice_s == "Home":
+    html_temp_home1 = """<div style="background-color:#454545;padding:10px">
+                              <h4 style="color:white;text-align:center;">
+                              Hand Sign recognition application using OpenCV, Streamlit.
+                              </h4>
+                              </div>
+                              </br>"""
+    st.markdown(html_temp_home1, unsafe_allow_html=True)
+
+elif choice_s == "Webcam Hand Detection":
+    RTC_CONFIGURATION = RTCConfiguration(
+        {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+    )
+    webrtc_ctx = webrtc_streamer(
+        key="WYH",
+        mode=WebRtcMode.SENDRECV,
+        rtc_configuration=RTC_CONFIGURATION,
+        media_stream_constraints={"video": True, "audio": False},
+        video_processor_factory=VideoProcessor,
+        async_processing=True,
+    )
